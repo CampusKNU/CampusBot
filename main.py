49f3d0e7d1ed26b5297a2c7fd4ead88ea55ecf78
mvper7 <denisdruzhko@gmail.com>
2024-03-20 00:30:59 +0200

    Init 1.0
    Created models and db communication by sqlalchemy

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+
+from src.handlers.user import router as user_router
+
+bot = Bot(getenv('API_TOKEN'))
+dp = Dispatcher()
+
+
+async def main() -> None:
+    dp.include_router(user_router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
